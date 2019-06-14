77f04e327e31a130662a24edb69d350d94714635
prateek27 <prateeknarang111@gmail.com>
2019-06-14 15:28:18 +0530

    added code

--- Lecture-03 FaceRecognition/face_recognition.py
+++ Lecture-03 FaceRecognition/face_recognition.py
@@ -61,10 +61,38 @@ Y = np.concatenate(labels,axis=0)
 print(X.shape)
 print(Y.shape)
 
-trainset = np.concatenate((X,Y),axis=1)
+#Training Set
+trainset = np.concatenate((X,Y.reshape(-1,1)),axis=1)
 
 while True:
-	#Prediction _____!!!
+	ret,frame = cam.read()
+	if ret==False:
+		print("Something Went Wrong!")
+		continue
+
+	key_pressed = cv2.waitKey(1)&0xFF #Bitmasking to get last 8 bits
+	if key_pressed==ord('q'): #ord-->ASCII Value(8 bit)
+		break
+	faces = face_cascade.detectMultiScale(frame,1.3,5)
+	if(len(faces)==0):
+		cv2.imshow("Faces Detected",frame)
+		continue
+
+	for face in faces:
+		x,y,w,h = face
+		face_section = frame[y-10:y+h+10,x-10:x+w+10];
+		face_section = cv2.resize(face_section,(100,100))
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+
+		pred = knn(trainset,face_section.flatten())
+		name = names[int(pred)]
+		cv2.putText(frame,name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+
+	cv2.imshow("Faces Detected",frame)
+
+cam.release()
+cv2.destroyAllWindows()
+
 	
 
 
